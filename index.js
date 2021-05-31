591db65c196a2b0891a9c8eb27b594a4d2e13ba3
Sukanya Sen <76467704+SukanyaSen530@users.noreply.github.com>
2021-05-31 12:02:14 +0530

    refactor, style- removed repetitive code, changed styles

--- index.js
+++ index.js
@@ -10,10 +10,22 @@ form.addEventListener('submit', (evt)=>{
     evt.preventDefault();
     const lang = form.elements.lang.value;
     const word = form.elements.word.value;
+    form.elements.word.value = '';
+    form.elements.lang.value = 'en_US';
     const sendURL = `${url}${lang}/${word}`;
     ulParent.innerHTML = "";
     const xhr = new XMLHttpRequest();
     xhr.open('GET', sendURL, true);
+    
+    function cli(word, ele) {
+        let li = document.createElement('li');
+        if (ele !== undefined) {
+            li.innerHTML = `<b>${word}</b> - <span>${ele}</span>`;
+        } else {
+            li.innerHTML = `<b>${word}</b> - Not Available :(</span>`;
+        }
+        return li;
+    }
 
     xhr.onload = () =>{
             if(xhr.status >= 200 && xhr.status < 400){
@@ -21,28 +33,22 @@ form.addEventListener('submit', (evt)=>{
                 console.log(res[0].word);
                 let ind = Math.floor(Math.random() * res[0].meanings.length);
                 p.innerHTML = "";
-                const {partOfSpeech, definitions} = res[0].meanings[ind];
+                const { partOfSpeech, definitions } = res[0].meanings[ind];
+                ulParent.append(cli("Word", res[0].word));
+                ulParent.appendChild(cli("Part Of Speech", partOfSpeech));
+                ulParent.appendChild(cli("Definition", definitions[0].definition));
+                ulParent.appendChild(cli("Example", definitions[0].example));
 
-                let li = document.createElement('li');
-                li.innerHTML = `<b>Part of Speech </b> - <span>${partOfSpeech}</span>`;
-                ulParent.appendChild(li);
-                let count=0;
-                definitions.forEach(ele =>{
-                    count++;
-                    let li1 = document.createElement('li'); 
-                    li1.innerHTML = `<b>Definition</b>(${count}) - <span>${ele.definition}</span>`;
-                   
-                    let li2 = document.createElement('li');
-                    if(ele.example !== undefined){
-                        li2.innerHTML = `<b>Example</b>(${count}) - <span>${ele.example}</span>`;
-                    }else{
-                        li2.innerHTML = `<b>Example</b>(${count}) - <span>Not Available :(</span>`;
-                    }
-                    
-                   
-                    ulParent.appendChild(li1);
-                    ulParent.appendChild(li2);
-                })  
+                const [sy1, sy2, sy3] = definitions[0].synonyms;
+                let li4 = document.createElement('li');
+                if (sy1 !== undefined && sy2 !== undefined && sy3 !== undefined) {
+                    li4.innerHTML = `<b>Synonyms</b> - <span>${sy1} , ${sy2} , ${sy3} !</span>`;
+                    ulParent.append(li4);
+                } else {
+                    li4.innerHTML = `<b>Synonyms!</b> - <span>Not Available</span>`;
+                    ulParent.append(li4);
+                }
+
             }else{
                 console.error("ERROR!", xhr.status); 
                 p.innerText = `Error :( Word not found! ${xhr.status}... Try searching a valid word!`;
@@ -50,30 +56,3 @@ form.addEventListener('submit', (evt)=>{
     }
     xhr.send();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-  //Mehnat
-  // meaning.forEach(ele => {
-                //     console.log(`Part of Speech - ${ele.partOfSpeech}`);
-                //     const definitions = ele.definitions;
-                //     definitions.forEach(def =>{
-                //         console.log(`Definition - ${def.definition}`);
-                //         //console.log(def.synonyms);
-                //         console.log(`Example - ${def.example}`);
-                //     })
-                // });
-
-                 //console.log(`Definition - ${ele.definition}`);
-                    //console.log(`Example - ${ele.example}`);
-                      //console.log(`Part of Speech - ${partOfSpeech}`);
